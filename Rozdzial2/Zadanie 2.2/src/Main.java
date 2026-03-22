fad3cef76774a5cfdbd3a2d13a6bb6d92c32c6bc
clavisray <matbog0@gmail.com>
2026-03-22 18:37:58 +0100

    Unfinished 2.2

--- Rozdzial2/Zadanie 2.2/src/Main.java
+++ Rozdzial2/Zadanie 2.2/src/Main.java
@@ -1,13 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        String a = JOptionPane.showInputDialog("Podaj a");
+        String b = JOptionPane.showInputDialog("Podaj b");
+        String c = JOptionPane.showInputDialog("Podaj c");
+
+
+    }
+}
+
+class Rownanie {
+    private double a, b, c;
+
+    // konstruktor
+    public Rownanie(double a, double b, double c) {
+        this.a = a; this.b = b; this.c = c;
+    }
+
+    // Nadanie wartosci wspolczynnikom
+    public void setWartosc(double a, double b, double c) {
+        this.a = a; this.b = b; this.c = c;
+    }
+
+    // Obliczenie Y dla podanego X
+    public double obliczY(double x) {
+        double y;
+        return y = a * Math.pow(x, 2) + b * x + c;
+    }
+
+    // Wyznaczenie liczby pierwiastkow
+    public int wyznaczPierwiastek() {
+        int p = 2;
+        double d = Math.pow(b, 2) - 4 * a * c;
+
+        if (d < 0) {
+            return 0;
+        } else if (d == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
