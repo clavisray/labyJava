ae1b71f3264bd684f4cd8ee4c52cd0d81fe9d433
clavisray <matbog0@gmail.com>
2026-03-23 20:06:52 +0100

    Finished to 2.2

--- Rozdzial2/Zadanie 2.2/src/Main.java
+++ Rozdzial2/Zadanie 2.2/src/Main.java
@@ -2,13 +2,53 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Podaj a");
-        String b = JOptionPane.showInputDialog("Podaj b");
-        String c = JOptionPane.showInputDialog("Podaj c");
+            Rownanie r = new Rownanie(0, 0, 0);
 
+            boolean running = true;
 
+            while (running){
+
+                String windowMessage = "Wybierz dzialanie 1-3\n" +
+                        "1 - Ustaw wartosci a,b,c\n" +
+                        "2 - Oblicz Y dla wskazanego X\n" +
+                        "3 - Wyznacz liczbe pierwiastkow rownania\n" +
+                        "4 - Wyjscie z programu\n";
+
+                String userInput = JOptionPane.showInputDialog(windowMessage);
+
+                switch (userInput) {
+                    case "1":
+                        double a = Double.parseDouble(JOptionPane.showInputDialog("Podaj a"));
+                        double b = Double.parseDouble(JOptionPane.showInputDialog("Podaj b"));
+                        double c = Double.parseDouble(JOptionPane.showInputDialog("Podaj c"));
+
+                        r.setWartosc(a, b, c);
+                        JOptionPane.showMessageDialog(null, "Ustawiono wartosci rownania.");
+                        break;
+
+                    case "2":
+                        double x = Double.parseDouble(JOptionPane.showInputDialog("Podaj X"));
+                        double y = r.obliczY(x);
+
+                        JOptionPane.showMessageDialog(null, "Y = " + y);
+                        break;
+
+                    case "3":
+                        int pierwiastek = r.wyznaczPierwiastek();
+
+                        JOptionPane.showMessageDialog(null, "Liczba pierwiastkow: " + pierwiastek);
+                        break;
+
+                    case "4":
+                        running = false;
+                        break;
+
+                    default:
+                        throw new IllegalArgumentException("Wybrano liczbę spoza zakresu. Wpisz ponownie: " + userInput);
+                }
+            }
+        }
     }
-}
 
 class Rownanie {
     private double a, b, c;
@@ -25,13 +65,11 @@ class Rownanie {
 
     // Obliczenie Y dla podanego X
     public double obliczY(double x) {
-        double y;
-        return y = a * Math.pow(x, 2) + b * x + c;
+        return a * Math.pow(x, 2) + b * x + c;
     }
 
     // Wyznaczenie liczby pierwiastkow
     public int wyznaczPierwiastek() {
-        int p = 2;
         double d = Math.pow(b, 2) - 4 * a * c;
 
         if (d < 0) {
