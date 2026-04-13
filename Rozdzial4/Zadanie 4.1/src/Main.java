e6a46c8b625056c098d6ba36043bb5739477d4e7
clavisray <matbog0@gmail.com>
2026-04-13 20:42:42 +0200

    4.1 done

--- Rozdzial4/Zadanie 4.1/src/Main.java
+++ Rozdzial4/Zadanie 4.1/src/Main.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
         String trescPliku = JOptionPane.showInputDialog(null, "Edytor tekstu, wprowadz wiadomosc, a nastepnie ja zapisz");
         String nazwaPliku = JOptionPane.showInputDialog(null, "Zapisz plik txt jako: ");
@@ -12,9 +14,20 @@ public class Main {
             writer.write(trescPliku);
         }
 
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(nazwaPliku + ".txt"), "UTF-8"));
-        String line = reader.readLine();
-        JOptionPane.showMessageDialog(null, line);
+        JFileChooser chooser = new JFileChooser();
+        int result = chooser.showOpenDialog(null);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(file), "UTF-8")
+            );
+
+            String line = reader.readLine();
+            JOptionPane.showMessageDialog(null, line);
+
+            reader.close();
+        }
     }
 }
