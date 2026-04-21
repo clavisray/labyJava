4642ad3e0a2d3bde6909082e6262a55f2183520e
clavisray <matbog0@gmail.com>
2026-04-21 21:30:17 +0200

    4.2 done

--- Rozdzial4/Zadanie 4.2/src/Main.java
+++ Rozdzial4/Zadanie 4.2/src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
+import java.io.*;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.GZIPInputStream;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        JFileChooser chooser = new JFileChooser();
+
+        // kompresja pliku do gz
+        chooser.showOpenDialog(null);
+        File file = chooser.getSelectedFile();
+
+        FileInputStream fis = new FileInputStream(file);
+        GZIPOutputStream gzip = new GZIPOutputStream(new FileOutputStream(file + ".gz"));
+
+        int bajt;
+        while ((bajt = fis.read()) != -1) {
+            gzip.write(bajt);
+        }
+
+        fis.close();
+        gzip.close();
+
+        // dekompresja pliku do txt
+        chooser.showOpenDialog(null);
+        file = chooser.getSelectedFile();
+
+        GZIPInputStream gis = new GZIPInputStream(new FileInputStream(file));
+        FileOutputStream fos = new FileOutputStream(file + ".txt");
+
+        while ((bajt = gis.read()) != -1) {
+            fos.write(bajt);
+        }
+
+        gis.close();
+        fos.close();
     }
 }
