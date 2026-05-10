8f2df326fb5a50cec5e2a6f6557cefefd4bc9ef7
clavisray <matbog0@gmail.com>
2026-05-10 14:08:25 +0200

    6.2 watek done with simple implementation

--- Rozdzial6/Zadanie 6.2/src/Main.java
+++ Rozdzial6/Zadanie 6.2/src/Main.java
@@ -10,10 +10,40 @@ class MapaKomunikatow {
     }
 }
 class Watek extends Thread {
+    private MapaKomunikatow mapako;
+    public String s;
+    public int start;
+    public Watek(MapaKomunikatow mapa, String s, int start) {
+        this.mapako = mapa;
+        this.s = s;
+        this.start = start;
+    }
 
+    public void run() {
+        try {
+            for (int i = start; i<=10; i+=2) {
+                mapako.dodaj(s + i, new Integer(i);
+                Thread.sleep(50);
+            }
+        }
+        catch (Exception e){};
+    }
 }
 public class Main {
     public static void main(String args[]) {
+        MapaKomunikatow mapa = new MapaKomunikatow();
 
+        Watek w1 = new Watek(mapa, "A", 1);
+        Watek w2 = new Watek(mapa, "B", 2);
+
+        w1.start();
+        w2.start();
+
+        try {
+            w1.join();
+            w2.join();
+        } catch (InterruptedException e){}
+
+        System.out.print(mapa);
     }
 }
