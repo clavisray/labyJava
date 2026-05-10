5eba53473e65902bef676fb9c3b7ee2da4156b2c
clavisray <matbog0@gmail.com>
2026-05-10 15:10:44 +0200

    7.2 starting point

--- Rozdzial7/Zadanie 7.2/src/Main.java
+++ Rozdzial7/Zadanie 7.2/src/Main.java
@@ -1,13 +1,107 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+//empty draft
+import java.net.*;
+import java.io.*;
+class Gracz implements Runnable {
+    private int plansza[][];
+    // liczba >= 100 to mina, 0, 1, ..., 8 -- ile wokól min
+    private boolean klikniete[][];
+    private PrintWriter out;
+    private BufferedReader in;
+    private Socket polaczenie;
+    private void InicjujPlansze() {
+        plansza = new int[11][11];
+        klikniete = new boolean[11][11];
+        int w, k, licznik;
+        licznik = 0;
+        while (licznik < 10) {
+            w = (int)(Math.random()*9) + 1;
+            k = (int)(Math.random()*9) + 1;
+            if (plansza[w][k] < 100) {
+                ++licznik;
+                plansza[w][k] = 100;
+                ++plansza[w-1][k-1];
+                ++plansza[w-1][k];
+                ++plansza[w-1][k+1];
+                ++plansza[w][k-1];
+                ++plansza[w][k+1];
+                ++plansza[w+1][k-1];
+                ++plansza[w+1][k];
+                ++plansza[w+1][k+1];
+            }
+        }
     }
+    public Gracz(Socket polaczenie) {
+        InicjujPlansze();
+        this.polaczenie = polaczenie;
+        try {
+            out = new PrintWriter(polaczenie.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(polaczenie.getInputStream()));
+        }
+        catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    public void run() {
+        int w, k, odkryte, liczba;
+        String wsp, odp;
+        boolean koniec = false;
+        odkryte=0;
+        try {
+            out.println("OK.");
+            while ((!koniec) && (odkryte<71)) {
+                wsp = in.readLine();
+                if (wsp == null) koniec = true;
+                else {
+                    try {
+                        liczba = Integer.parseInt(wsp);
+                        w = (int)((liczba-1)/9) + 1;
+                        k = (liczba-1) % 9 + 1;
+                    }
+                    catch (NumberFormatException e) {
+                        w = 200;
+                        k = 200;
+                    }
+                    if ((w>=1) && (w<=9) && (k>=1) && (k<=9)) {
+                        if (plansza[w][k] >= 100) {
+                            out.println("bum");
+                            koniec = true;
+                        }
+                        else {
+                            out.println(Integer.toString(plansza[w][k]));
+                            if (!klikniete[w][k]) ++odkryte;
+                        }
+                        klikniete[w][k] = true;
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        finally {
+            try {
+                polaczenie.close();
+            }
+            catch (IOException e) {}
+        }
+    }
+}
+public class Serwer {
+    public static void main(String[] args) {
+        ServerSocket server;
+        try {
+            server = new ServerSocket(9696);
+
+        while(true) {
+            Socket polaczenie = server.accept();
+            Thread t = new Thread(
+                    new Gracz(polaczenie));
+            t.start();
+        }
+    }
+catch (IOException e) {
+        System.out.println(e.toString());
+    }
+}
 }
