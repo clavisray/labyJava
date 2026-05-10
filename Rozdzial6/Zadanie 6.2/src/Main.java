6a36d3ea2daf9961dfc872f42d619939796e4a89
clavisray <matbog0@gmail.com>
2026-05-10 13:52:54 +0200

    6.2 hashmap class done

--- Rozdzial6/Zadanie 6.2/src/Main.java
+++ Rozdzial6/Zadanie 6.2/src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
+class MapaKomunikatow {
+    private HashMap mapa = new HashMap();
+    public synchronized void dodaj(String klucz, Integer wartosc) {
+        mapa.put(klucz, wartosc);
+    }
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public Integer pobierz(String klucz) {
+        return (Integer) mapa.get(klucz);
+    }
+}
+class Watek extends Thread {
+
+}
+public class Main {
+    public static void main(String args[]) {
+
     }
 }
